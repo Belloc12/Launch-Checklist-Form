09f1df3a49ff1f19354d3b31699e77307ebd458d
georgenorris <georgenorris@stlf14-2.local>
2019-10-28 18:39:49 -0500

    finished product

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 // Write your JavaScript code here!
+function init(){
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+        response.json().then(function(json) {
+         const destination = document.getElementById("missionTarget");
+         destination.innerHTML = `        
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[3].name}</li>
+               <li>Diameter: ${json[3].diameter}</li>
+               <li>Star: ${json[3].star}</li>
+               <li>Distance from Earth: ${json[3].distance}</li>
+               <li>Number of Moons: ${json[3].moons}</li>
+            </ol>
+            <img src="${json[3].image}"></img>
+
+         `;
+      });
+   }); 
+};
+
+window.onload = init;
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
@@ -11,3 +32,5 @@
 </ol>
 <img src="${}">
 */
+
+
